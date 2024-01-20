b1e8c925bc91d4f8a268b2a98ca1552aecc3d6cb
Albert Boehmler <ajboehmler@gmail.com>
2024-01-20 18:51:19 -0500

    Builds from new docker PB compiler image

--- py_qroma/qroma_comm/qcio_serial_old.py
+++ py_qroma/qroma_comm/qcio_serial_old.py
@@ -1,82 +1,82 @@
-import asyncio
-import serial
-import time
-import base64
-from dataclasses import dataclass
-from . import qcio_base
-
-
-@dataclass
-class QcioSerialConfig:
-    com_port: str
-    baud_rate: int = 115200
-    timeout: float = 0.5
-
-
-class QcioSerial(qcio_base.QcioBase):
-    ser: serial.Serial
-
-    ser_timeout: float
-
-    def __init__(self, arg: str | QcioSerialConfig):
-        self.ser = self._create_serial(arg)
-        self._keep_running = True
-
-    def _create_serial(self, arg: str | QcioSerialConfig):
-        if isinstance(arg, str):
-            return self._create_serial(QcioSerialConfig(arg))
-        else:
-            self.ser_timeout = arg.timeout
-            return serial.Serial(
-                port=arg.com_port,
-                baudrate=arg.baud_rate,
-                # timeout=arg.timeout,
-                timeout=0.1,
-            )
-
-    async def send_bytes(self, cmd_bytes: bytes):
-        def do_send(the_bytes):
-            print(f"SENDING {len(the_bytes)} BYTES")
-            self.ser.write(the_bytes)
-            self.ser.flush()
-
-        send_count = 1000
-        sleep_duration = 0.03
-
-        bytes_to_send = cmd_bytes[:]
-        print(f"TO SEND: {len(bytes_to_send)} BYTES")
-        while len(bytes_to_send) > send_count:
-            the_bytes = bytes_to_send[0:send_count]
-            do_send(the_bytes)
-            bytes_to_send = bytes_to_send[send_count:]
-            time.sleep(sleep_duration)
-
-        if len(bytes_to_send) > 0:
-            do_send(bytes_to_send)
-
-    async def send_bytes_base64_with_newline(self, cmd_bytes: bytes):
-        b64_cmd_bytes = base64.b64encode(cmd_bytes) + b"\n"
-        print(f"SENDING B64: {b64_cmd_bytes}")
-        await self.send_bytes(b64_cmd_bytes)
-
-    async def read_next_byte(self, timeout: float):
-        return await self.read_n_bytes(1, timeout)
-
-    async def read_n_bytes(self, byte_count: int, timeout: float):
-        """
-        Wait up until timeout seconds to receive a byte.
-        """
-        self.ser.timeout = timeout
-        n_bytes = self.ser.read(byte_count)
-        print(f"READx BYTES: {n_bytes}")
-        return n_bytes
-
-    async def run(self):
-        while self._keep_running:
-            await asyncio.sleep(0.01)
-
-    def stop(self):
-        self._keep_running = False
-
-    async def is_ready(self):
-        return True
+# import asyncio
+# import serial
+# import time
+# import base64
+# from dataclasses import dataclass
+# from . import qcio_base
+#
+#
+# @dataclass
+# class QcioSerialConfig:
+#     com_port: str
+#     baud_rate: int = 115200
+#     timeout: float = 0.5
+#
+#
+# class QcioSerial(qcio_base.QcioBase):
+#     ser: serial.Serial
+#
+#     ser_timeout: float
+#
+#     def __init__(self, arg: str | QcioSerialConfig):
+#         self.ser = self._create_serial(arg)
+#         self._keep_running = True
+#
+#     def _create_serial(self, arg: str | QcioSerialConfig):
+#         if isinstance(arg, str):
+#             return self._create_serial(QcioSerialConfig(arg))
+#         else:
+#             self.ser_timeout = arg.timeout
+#             return serial.Serial(
+#                 port=arg.com_port,
+#                 baudrate=arg.baud_rate,
+#                 # timeout=arg.timeout,
+#                 timeout=0.1,
+#             )
+#
+#     async def send_bytes(self, cmd_bytes: bytes):
+#         def do_send(the_bytes):
+#             print(f"SENDING {len(the_bytes)} BYTES")
+#             self.ser.write(the_bytes)
+#             self.ser.flush()
+#
+#         send_count = 1000
+#         sleep_duration = 0.03
+#
+#         bytes_to_send = cmd_bytes[:]
+#         print(f"TO SEND: {len(bytes_to_send)} BYTES")
+#         while len(bytes_to_send) > send_count:
+#             the_bytes = bytes_to_send[0:send_count]
+#             do_send(the_bytes)
+#             bytes_to_send = bytes_to_send[send_count:]
+#             time.sleep(sleep_duration)
+#
+#         if len(bytes_to_send) > 0:
+#             do_send(bytes_to_send)
+#
+#     async def send_bytes_base64_with_newline(self, cmd_bytes: bytes):
+#         b64_cmd_bytes = base64.b64encode(cmd_bytes) + b"\n"
+#         print(f"SENDING B64: {b64_cmd_bytes}")
+#         await self.send_bytes(b64_cmd_bytes)
+#
+#     async def read_next_byte(self, timeout: float):
+#         return await self.read_n_bytes(1, timeout)
+#
+#     async def read_n_bytes(self, byte_count: int, timeout: float):
+#         """
+#         Wait up until timeout seconds to receive a byte.
+#         """
+#         self.ser.timeout = timeout
+#         n_bytes = self.ser.read(byte_count)
+#         print(f"READx BYTES: {n_bytes}")
+#         return n_bytes
+#
+#     async def run(self):
+#         while self._keep_running:
+#             await asyncio.sleep(0.01)
+#
+#     def stop(self):
+#         self._keep_running = False
+#
+#     async def is_ready(self):
+#         return True
--- py_qroma/qroma_comm_proto/qroma_comm_pb2.py
+++ py_qroma/qroma_comm_proto/qroma_comm_pb2.py
@@ -3,229 +3,29 @@
 # source: qroma-comm.proto
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
+from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-import file_system_commands_pb2 as file__system__commands__pb2
-import qroma_comm_config_commands_pb2 as qroma__comm__config__commands__pb2
+from . import file_system_commands_pb2 as file__system__commands__pb2
+from . import qroma_comm_config_commands_pb2 as qroma__comm__config__commands__pb2
 
 
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='qroma-comm.proto',
-  package='',
-  syntax='proto3',
-  serialized_options=None,
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x10qroma-comm.proto\x1a\x1a\x66ile-system-commands.proto\x1a qroma-comm-config-commands.proto\"\x97\x01\n\x10QromaCommCommand\x12\x19\n\x0f\x61ppCommandBytes\x18\x01 \x01(\x0cH\x00\x12\'\n\tfsCommand\x18\x02 \x01(\x0b\x32\x12.FileSystemCommandH\x00\x12\x34\n\x11\x63ommConfigCommand\x18\x03 \x01(\x0b\x32\x17.QromaCommConfigCommandH\x00\x42\t\n\x07\x63ommand\"H\n\x1aQromaCommHeartbeatResponse\x12\x12\n\nuptimeInMs\x18\x01 \x01(\r\x12\x16\n\x0eheartbeatTicks\x18\x02 \x01(\r\"\xd8\x01\n\x11QromaCommResponse\x12\x1a\n\x10\x61ppResponseBytes\x18\x01 \x01(\x0cH\x00\x12)\n\nfsResponse\x18\x02 \x01(\x0b\x32\x13.FileSystemResponseH\x00\x12\x36\n\x12\x63ommConfigResponse\x18\x03 \x01(\x0b\x32\x18.QromaCommConfigResponseH\x00\x12\x38\n\x11heartbeatResponse\x18\x04 \x01(\x0b\x32\x1b.QromaCommHeartbeatResponseH\x00\x42\n\n\x08responseb\x06proto3'
-  ,
-  dependencies=[file__system__commands__pb2.DESCRIPTOR,qroma__comm__config__commands__pb2.DESCRIPTOR,])
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10qroma-comm.proto\x1a\x1a\x66ile-system-commands.proto\x1a qroma-comm-config-commands.proto\"\x97\x01\n\x10QromaCommCommand\x12\x19\n\x0f\x61ppCommandBytes\x18\x01 \x01(\x0cH\x00\x12\'\n\tfsCommand\x18\x02 \x01(\x0b\x32\x12.FileSystemCommandH\x00\x12\x34\n\x11\x63ommConfigCommand\x18\x03 \x01(\x0b\x32\x17.QromaCommConfigCommandH\x00\x42\t\n\x07\x63ommand\"H\n\x1aQromaCommHeartbeatResponse\x12\x12\n\nuptimeInMs\x18\x01 \x01(\r\x12\x16\n\x0eheartbeatTicks\x18\x02 \x01(\r\"\xd8\x01\n\x11QromaCommResponse\x12\x1a\n\x10\x61ppResponseBytes\x18\x01 \x01(\x0cH\x00\x12)\n\nfsResponse\x18\x02 \x01(\x0b\x32\x13.FileSystemResponseH\x00\x12\x36\n\x12\x63ommConfigResponse\x18\x03 \x01(\x0b\x32\x18.QromaCommConfigResponseH\x00\x12\x38\n\x11heartbeatResponse\x18\x04 \x01(\x0b\x32\x1b.QromaCommHeartbeatResponseH\x00\x42\n\n\x08responseb\x06proto3')
 
-
-
-
-_QROMACOMMCOMMAND = _descriptor.Descriptor(
-  name='QromaCommCommand',
-  full_name='QromaCommCommand',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='appCommandBytes', full_name='QromaCommCommand.appCommandBytes', index=0,
-      number=1, type=12, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"",
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='fsCommand', full_name='QromaCommCommand.fsCommand', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='commConfigCommand', full_name='QromaCommCommand.commConfigCommand', index=2,
-      number=3, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-    _descriptor.OneofDescriptor(
-      name='command', full_name='QromaCommCommand.command',
-      index=0, containing_type=None,
-      create_key=_descriptor._internal_create_key,
-    fields=[]),
-  ],
-  serialized_start=83,
-  serialized_end=234,
-)
-
-
-_QROMACOMMHEARTBEATRESPONSE = _descriptor.Descriptor(
-  name='QromaCommHeartbeatResponse',
-  full_name='QromaCommHeartbeatResponse',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='uptimeInMs', full_name='QromaCommHeartbeatResponse.uptimeInMs', index=0,
-      number=1, type=13, cpp_type=3, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='heartbeatTicks', full_name='QromaCommHeartbeatResponse.heartbeatTicks', index=1,
-      number=2, type=13, cpp_type=3, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=236,
-  serialized_end=308,
-)
-
-
-_QROMACOMMRESPONSE = _descriptor.Descriptor(
-  name='QromaCommResponse',
-  full_name='QromaCommResponse',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='appResponseBytes', full_name='QromaCommResponse.appResponseBytes', index=0,
-      number=1, type=12, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"",
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='fsResponse', full_name='QromaCommResponse.fsResponse', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='commConfigResponse', full_name='QromaCommResponse.commConfigResponse', index=2,
-      number=3, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='heartbeatResponse', full_name='QromaCommResponse.heartbeatResponse', index=3,
-      number=4, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-    _descriptor.OneofDescriptor(
-      name='response', full_name='QromaCommResponse.response',
-      index=0, containing_type=None,
-      create_key=_descriptor._internal_create_key,
-    fields=[]),
-  ],
-  serialized_start=311,
-  serialized_end=527,
-)
-
-_QROMACOMMCOMMAND.fields_by_name['fsCommand'].message_type = file__system__commands__pb2._FILESYSTEMCOMMAND
-_QROMACOMMCOMMAND.fields_by_name['commConfigCommand'].message_type = qroma__comm__config__commands__pb2._QROMACOMMCONFIGCOMMAND
-_QROMACOMMCOMMAND.oneofs_by_name['command'].fields.append(
-  _QROMACOMMCOMMAND.fields_by_name['appCommandBytes'])
-_QROMACOMMCOMMAND.fields_by_name['appCommandBytes'].containing_oneof = _QROMACOMMCOMMAND.oneofs_by_name['command']
-_QROMACOMMCOMMAND.oneofs_by_name['command'].fields.append(
-  _QROMACOMMCOMMAND.fields_by_name['fsCommand'])
-_QROMACOMMCOMMAND.fields_by_name['fsCommand'].containing_oneof = _QROMACOMMCOMMAND.oneofs_by_name['command']
-_QROMACOMMCOMMAND.oneofs_by_name['command'].fields.append(
-  _QROMACOMMCOMMAND.fields_by_name['commConfigCommand'])
-_QROMACOMMCOMMAND.fields_by_name['commConfigCommand'].containing_oneof = _QROMACOMMCOMMAND.oneofs_by_name['command']
-_QROMACOMMRESPONSE.fields_by_name['fsResponse'].message_type = file__system__commands__pb2._FILESYSTEMRESPONSE
-_QROMACOMMRESPONSE.fields_by_name['commConfigResponse'].message_type = qroma__comm__config__commands__pb2._QROMACOMMCONFIGRESPONSE
-_QROMACOMMRESPONSE.fields_by_name['heartbeatResponse'].message_type = _QROMACOMMHEARTBEATRESPONSE
-_QROMACOMMRESPONSE.oneofs_by_name['response'].fields.append(
-  _QROMACOMMRESPONSE.fields_by_name['appResponseBytes'])
-_QROMACOMMRESPONSE.fields_by_name['appResponseBytes'].containing_oneof = _QROMACOMMRESPONSE.oneofs_by_name['response']
-_QROMACOMMRESPONSE.oneofs_by_name['response'].fields.append(
-  _QROMACOMMRESPONSE.fields_by_name['fsResponse'])
-_QROMACOMMRESPONSE.fields_by_name['fsResponse'].containing_oneof = _QROMACOMMRESPONSE.oneofs_by_name['response']
-_QROMACOMMRESPONSE.oneofs_by_name['response'].fields.append(
-  _QROMACOMMRESPONSE.fields_by_name['commConfigResponse'])
-_QROMACOMMRESPONSE.fields_by_name['commConfigResponse'].containing_oneof = _QROMACOMMRESPONSE.oneofs_by_name['response']
-_QROMACOMMRESPONSE.oneofs_by_name['response'].fields.append(
-  _QROMACOMMRESPONSE.fields_by_name['heartbeatResponse'])
-_QROMACOMMRESPONSE.fields_by_name['heartbeatResponse'].containing_oneof = _QROMACOMMRESPONSE.oneofs_by_name['response']
-DESCRIPTOR.message_types_by_name['QromaCommCommand'] = _QROMACOMMCOMMAND
-DESCRIPTOR.message_types_by_name['QromaCommHeartbeatResponse'] = _QROMACOMMHEARTBEATRESPONSE
-DESCRIPTOR.message_types_by_name['QromaCommResponse'] = _QROMACOMMRESPONSE
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
-QromaCommCommand = _reflection.GeneratedProtocolMessageType('QromaCommCommand', (_message.Message,), {
-  'DESCRIPTOR' : _QROMACOMMCOMMAND,
-  '__module__' : 'qroma_comm_pb2'
-  # @@protoc_insertion_point(class_scope:QromaCommCommand)
-  })
-_sym_db.RegisterMessage(QromaCommCommand)
-
-QromaCommHeartbeatResponse = _reflection.GeneratedProtocolMessageType('QromaCommHeartbeatResponse', (_message.Message,), {
-  'DESCRIPTOR' : _QROMACOMMHEARTBEATRESPONSE,
-  '__module__' : 'qroma_comm_pb2'
-  # @@protoc_insertion_point(class_scope:QromaCommHeartbeatResponse)
-  })
-_sym_db.RegisterMessage(QromaCommHeartbeatResponse)
-
-QromaCommResponse = _reflection.GeneratedProtocolMessageType('QromaCommResponse', (_message.Message,), {
-  'DESCRIPTOR' : _QROMACOMMRESPONSE,
-  '__module__' : 'qroma_comm_pb2'
-  # @@protoc_insertion_point(class_scope:QromaCommResponse)
-  })
-_sym_db.RegisterMessage(QromaCommResponse)
-
-
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'qroma_comm_pb2', _globals)
+if _descriptor._USE_C_DESCRIPTORS == False:
+  DESCRIPTOR._options = None
+  _globals['_QROMACOMMCOMMAND']._serialized_start=83
+  _globals['_QROMACOMMCOMMAND']._serialized_end=234
+  _globals['_QROMACOMMHEARTBEATRESPONSE']._serialized_start=236
+  _globals['_QROMACOMMHEARTBEATRESPONSE']._serialized_end=308
+  _globals['_QROMACOMMRESPONSE']._serialized_start=311
+  _globals['_QROMACOMMRESPONSE']._serialized_end=527
 # @@protoc_insertion_point(module_scope)
